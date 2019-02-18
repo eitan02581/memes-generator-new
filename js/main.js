f9ac20f0080af39316e6bd3cb2bfd70546ed6d73
Eitan Elnekave <eitan02581@gmail.com>
2019-02-18 14:01:18 +0200

    cleaning the code

--- js/main.js
+++ js/main.js
@@ -5,9 +5,10 @@ function init() {
 function onDispCanvas(imgId) {
     initCanvas()
     convertImageToCanvas(imgId)
+    console.log(document.querySelector('.gallery-section'));
+    
     document.querySelector('.canvas-section').classList.remove('hidden');
     document.querySelector('.gallery-section').classList.add('hidden');
-
 }
 
 function onDispGallerry() {
